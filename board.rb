fa3a0a81003ee2c2fd6fd018ce192859b0de8c8b
Morghan <morghan102@gmail.com>
2020-09-10 17:41:00 -0500

    finished file class. beginning instance methods of board class

--- board.rb
+++ board.rb
@@ -1,5 +1,21 @@
+require_relative "./tile.rb"
+require "byebug"
+
+
 class Board
 
+  attr_accessor :grid
 
+  def initialize
+    @grid = grid
+  end
+
+  def self.from_file(filename) #class method... factory method?
+    File.readlines(filename).map do |row|
+      row.chomp.split("").map do |tile|
+        Tile.new(tile.to_i)
+      end
+    end
+  end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,5 +1,30 @@
 class Tile
 
+  attr_accessor :given, :value
+  def initialize(value)
+    @given = value == 0 ? true : false
+    @value = value
+  end
+
+  def to_s 
+    @value == 0 ? " " : @value.to_s.colorize #how to use that plugin?
+  end
+
+  def value=(new_val)
+    if @given
+      # they have "if given?" and have a def given? / @given / end
+      puts "You can't change this value."
+    else 
+      @value = new_val
+    end
+  end
+
+  # HOW DOES THIS WORK????
+  # they have given?
+  def color 
+    @given ? :blue : :red
+  end
 
 
+  
 end
